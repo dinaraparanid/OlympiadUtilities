b7a50337155e10441922f9d3e0b84b595c1c1b49
Paranid5 <58735614+dinaraparanid@users.noreply.github.com>
2020-10-19 12:52:13 +0500

    Update Kombinatorics.h

--- Kombinatorics.h
+++ Kombinatorics.h
@@ -1,25 +1,24 @@
 #pragma once
-#include <vector>
 
 template <typename T>
-extern inline size_t factorial (T n) // факториал
+constexpr size_t factorial(const T n) // факториал
 {
 	size_t ans = 1;
 
-	for (T i = 2; i <= n; i++)
+	for (T i = 2; i <= n; ++i)
 		ans *= i;
 
 	return ans;
 }
 
 template <typename T>
-extern inline size_t C (T n, T k) // Сочетания
+constexpr size_t C(const T n, const T k) // Сочетания
 {
 	return factorial(n) / factorial(k) / factorial(n - k);
 }
 
 template <typename T>
-extern inline size_t A (T n, T k) // Размещения
+constexpr size_t A(const T n, const T k) // Размещения
 {
 	return factorial(n) / factorial(n - k);
 }
