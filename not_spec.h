3c07194d016b51e9431d00623011603c29db385d
Paranid5 <58735614+dinaraparanid@users.noreply.github.com>
2020-10-20 23:55:16 +0500

    Create not_spec.h

---
+++ not_spec.h
@@ -0,0 +1,48 @@
+#pragma once 
+#include <functional>
+
+/** Бинарный поиск
+@param a - начало (наименьшая граница)
+@param b - конец (наибольшая граница)
+@param fn - функция, по которой выполняем бин.поиск
+@param cnst - константа, при которой обрывается поиск (по-умолчанию = 1)
+@return искомый элемент
+
+@example:
+// считаем, что всё подключенно
+
+int main()
+{
+  // ищем 3 среди множества от 0 до 10
+	std::printf("%d", bin_search<int>(0, 10,[](const int a) {
+	  if (a > 3) return 1;
+	  if (a < 3) return -1;
+	  return 0;
+	}));
+	return 0;
+}
+*/
+
+template <typename T> T bin_search(T a, T b, std::function<int(const T)> fn, double cnst = 1)
+{
+	T start = a, finish = b;
+
+	while (finish - start > cnst)
+	{
+		const T middle = (start + finish) / 2;
+
+		switch (fn(middle))
+		{
+		case 1:
+			finish = middle;
+			break;
+		case -1:
+			start = middle;
+			break;
+		default:
+			return middle;
+		}
+	}
+
+	return start;
+}
