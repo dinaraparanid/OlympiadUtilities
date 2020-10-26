e2d2b5049d30baefc03b0252bfcd1f4cc229d971
Paranid5 <58735614+dinaraparanid@users.noreply.github.com>
2020-10-26 15:31:36 +0500

    Update Strings.h

--- Strings.h
+++ Strings.h
@@ -38,3 +38,50 @@ std::vector<int> pref (std::string& s)
 
 	return p;
 }
+
+// Z-функция
+
+std::vector<size_t> z_func(const std::string& str)
+{
+	const size_t len = str.size();
+	std::vector<size_t> z(len);
+
+	if (0 == len)
+		return z;
+
+	z[0] = len;
+
+	for (size_t cur = 1, left = 0, right = 1; cur < len; ++cur)
+	{
+		if (cur >= right)
+		{
+			size_t off = 0;
+			while (cur + off < len && str[cur + off] == str[off])
+				++off;
+
+			z[cur] = off;
+			right = cur + z[cur];
+			left = cur;
+		}
+
+		else
+		{
+			const size_t equiv = cur - left;
+
+			if (z[equiv] < right - cur)
+				z[cur] = z[equiv];
+
+			else
+			{
+				size_t off = 0;
+				while (right + off < len && str[right - cur + off] == str[right + off])
+					++off;
+				z[cur] = right - cur + off;
+				right += off;
+				left = cur;
+			}
+		}
+	}
+
+	return z;
+}
