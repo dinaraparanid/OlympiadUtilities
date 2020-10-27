a91744e9c9413d6125098ca90faa9fbee8792f2d
Paranid5 <58735614+dinaraparanid@users.noreply.github.com>
2020-10-27 15:44:47 +0500

    Update Strings.h

--- Strings.h
+++ Strings.h
@@ -14,7 +14,9 @@ extern inline std::vector<size_t> prefix_function(const std::string& s)
 		while (j > 0 && s[i] != s[j])
 			j = pi[j - 1];
 		
-		if (s[i] == s[j])  ++j;
+		if (s[i] == s[j])
+			++j;
+		
 		pi[i] = j;
 	}
 	
@@ -39,4 +41,42 @@ extern inline std::vector<size_t> z_function(const std::string& s)
 	}
 	
 	return z;
+}
+
+extern inline std::pair<std::vector<size_t>, std::vector<size_t>> manaker(const std::string& s)
+{
+	const size_t size = s.length();
+	std::vector<size_t> d1(size); // палиндромы нечётной длины 
+	
+	size_t l = 0, r = -1;
+	
+	for (size_t i = 0; i < size; ++i)
+	{
+		size_t k = i > r ? 1 : std::min(d1[l + r - i], r - i + 1);
+		
+		while (i + k < size && i - k >= 0 && s[i + k] == s[i - k])  ++k;
+		
+		d1[i] = k;
+		
+		if (i + k - 1 > r)
+			l = i - k + 1, r = i + k - 1;
+	}
+
+	std::vector<size_t> d2(size); // палиндромы чётной длины 
+	
+	l = 0, r = -1;
+	
+	for (size_t i = 0; i < size; ++i)
+	{
+		size_t k = i > r ? 0 : std::min(d2[l + r - i + 1], r - i + 1);
+		
+		while (i + k < size && i - k - 1 >= 0 && s[i + k] == s[i - k - 1])  ++k;
+		
+		d2[i] = k;
+		
+		if (i + k - 1 > r)
+			l = i - k, r = i + k - 1;
+	}
+
+	return std::make_pair(d1, d2);
 }
