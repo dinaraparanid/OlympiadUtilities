09df17dd4f00554d3199cc2dcf8e50c9039b0dde
Paranid5 <58735614+dinaraparanid@users.noreply.github.com>
2020-12-01 00:24:28 +0500

    hash

--- Strings.h
+++ Strings.h
@@ -81,3 +81,35 @@ extern inline std::pair<std::vector<int>, std::vector<int>> manaker(const std::s
 
 	return std::make_pair(d1, d2);
 }
+
+
+inline std::vector<int64_t> getHash(const std::string& s, const int p, const int mod)
+{
+	std::vector<int64_t> ans(s.length());
+	ans[0] = s[0];
+
+	for (int i = 1; i < s.length(); i++)
+		ans[i] = (ans[i - 1] * p + s[i]) % mod;
+
+	return ans;
+}
+
+inline std::vector<int64_t> powers(const int p, const int mod, const int size) {
+	std::vector<int64_t> ans(size); ans[0] = 1;
+
+	for (int i = 1; i < size; i++)
+		ans[i] = (ans[i - 1] * 1LL * p) % mod;
+
+	return ans;
+}
+
+inline int64_t subHash(const int l, const int r, const int mod, const std::vector<int64_t>& hash, const std::vector<int64_t>& powers)
+{
+	const int64_t big = hash[r];
+	int64_t small = 0;
+
+	if (l != 0)
+		small = (hash[l - 1] * 1LL * powers[r - l + 1]) % mod;
+
+	return (big - small + mod) % mod;
+}
